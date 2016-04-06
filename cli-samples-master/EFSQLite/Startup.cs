d3e4cc5ae04c587840c8dde9cd4ca77d8cd27f53
Don Schenck <don.schenck@gmail.com>
2016-04-06 11:12:23 -0400

    More fixes to EFSQLite

--- cli-samples-master/EFSQLite/Startup.cs
+++ cli-samples-master/EFSQLite/Startup.cs
@@ -10,7 +10,7 @@ namespace EFSQLite
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
