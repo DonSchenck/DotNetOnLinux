6fd1fffd8e9e778b4b6c218f97df59aebeb8bbb2
Don Schenck <don.schenck@gmail.com>
2016-04-06 11:13:57 -0400

    Another attempt

--- cli-samples-master/EFSQLite/Startup.cs
+++ cli-samples-master/EFSQLite/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace EFSQLite
 {
@@ -10,7 +11,7 @@ namespace EFSQLite
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddMvc();
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
