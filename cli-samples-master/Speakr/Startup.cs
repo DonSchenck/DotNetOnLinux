149fb11202dc5d267ddf56d0ffa387e0bcaf3e4c
Don Schenck <don.schenck@gmail.com>
2016-04-07 17:10:18 -0400

    Speakr work

--- cli-samples-master/Speakr/Startup.cs
+++ cli-samples-master/Speakr/Startup.cs
@@ -5,7 +5,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.MvcServiceCollectionExtensions;
 using Microsoft.Extensions.Logging;
 
 namespace Speakr
