cb0c7e8d07af01b62191d6c5bfb00009f61e85c8
Don Schenck <don.schenck@gmail.com>
2016-04-07 17:49:35 -0400

    More Speakr debugging

---
+++ cli-samples-master/Speakr/Program.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace Speakr
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseDefaultHostingConfiguration(args)
+                        .UseIIS()
+                        .UseStartup<Startup>().UseUrls("http://*:5000")
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
