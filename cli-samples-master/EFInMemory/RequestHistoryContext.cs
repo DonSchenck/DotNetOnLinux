2ebb47d9aa33b97d707c32b159da4524f1c115a3
Don Schenck <don.schenck@gmail.com>
2016-04-07 16:55:20 -0400

    Another bug squashed

--- cli-samples-master/EFInMemory/RequestHistoryContext.cs
+++ cli-samples-master/EFInMemory/RequestHistoryContext.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EFSQLite
+namespace EFInMemory
 {
     public class RequestHistoryContext : DbContext
     {
