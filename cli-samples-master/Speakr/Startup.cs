4ae514ca62f7e6afb15145180b9b0b038e282bd7
Don Schenck <don.schenck@gmail.com>
2016-04-07 17:15:50 -0400

    Fixed Speakr Startup.cs

--- cli-samples-master/Speakr/Startup.cs
+++ cli-samples-master/Speakr/Startup.cs
@@ -1,12 +1,13 @@
 ﻿//using System;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 //using System.Linq;
 //using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection.MvcServiceCollectionExtensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Speakr
 {
