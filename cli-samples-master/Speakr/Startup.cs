10717b5beccab155e95a320d1798921589a6add6
Don Schenck <don.schenck@gmail.com>
2016-04-08 09:43:58 -0400

    Changed initial page to Submissions

--- cli-samples-master/Speakr/Startup.cs
+++ cli-samples-master/Speakr/Startup.cs
@@ -69,7 +69,7 @@ namespace Speakr
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Submissions}/{action=Index}/{id?}");
             });
         }
 
