110f39b70e68919002ccac7dcdf7ddde90fac52a
Don Schenck <don.schenck@gmail.com>
2016-04-19 20:51:41 -0400

    Fixed Speakr to use SQLite

--- cli-samples-master/Speakr/Startup.cs
+++ cli-samples-master/Speakr/Startup.cs
@@ -4,30 +4,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Framework.DependencyInjection;
 
 namespace Speakr
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();
-
-            //if (env.IsDevelopment())
-            //{
-            //    // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-            //    builder.AddApplicationInsightsSettings(developerMode: true);
-            //}
-            //Configuration = builder.Build();
-        }
-
-        //public IConfigurationRoot Configuration { get; set; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -39,23 +20,6 @@ namespace Speakr
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(LogLevel.Debug);
-            //loggerFactory.AddDebug();
-
-            //app.UseApplicationInsightsRequestTelemetry();
-
-            if (env.IsDevelopment())
-            {
-                //                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            //app.UseIISPlatformHandler();
-
-            //app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseStaticFiles();
 
@@ -67,7 +31,5 @@ namespace Speakr
             });
         }
 
-        // Entry point for the application.
-        //public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
