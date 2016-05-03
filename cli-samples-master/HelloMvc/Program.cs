86359137e2c14563f43556c026497909ddc211da
Don Schenck <don.schenck@gmail.com>
2016-05-03 11:13:19 -0400

    HelloMvc fixed

--- cli-samples-master/HelloMvc/Program.cs
+++ cli-samples-master/HelloMvc/Program.cs
@@ -11,7 +11,6 @@ namespace HelloMvc
             var host = new WebHostBuilder()
                         .UseKestrel()
                         .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseDefaultHostingConfiguration(args)
                         .UseIISIntegration()
                         .UseStartup<Startup>().UseUrls("http://*:5000")
                         .Build();
