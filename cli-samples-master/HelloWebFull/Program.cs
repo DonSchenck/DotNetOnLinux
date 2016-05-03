f531c62642583785413251e36f5eda88d48d3bb3
Don Schenck <don.schenck@gmail.com>
2016-05-03 10:59:03 -0400

    Fixes for UseDefaultHostingConfiguration

--- cli-samples-master/HelloWebFull/Program.cs
+++ cli-samples-master/HelloWebFull/Program.cs
@@ -10,7 +10,6 @@ namespace HelloWeb
             var host = new WebHostBuilder()
                         .UseKestrel()
                         .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseDefaultHostingConfiguration(args)
                         .UseIISIntegration()
                         .UseStartup<Startup>().UseUrls("http://*:5000")
                         .Build();
