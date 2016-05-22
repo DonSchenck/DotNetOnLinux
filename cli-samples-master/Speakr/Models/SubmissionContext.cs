e92d7551967597560b8146708008c32a177c9654
Don Schenck <dschenck@localhost.localdomain>
2016-05-22 01:08:02 -0400

    Changes for RC2

--- cli-samples-master/Speakr/Models/SubmissionContext.cs
+++ cli-samples-master/Speakr/Models/SubmissionContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.SqlServer;
 //using Microsoft.EntityFrameworkCore.Sqlite;
 
 namespace Speakr.Models
@@ -11,10 +12,12 @@ namespace Speakr.Models
     {
         public DbSet<Submission> Submissions { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //            builder.UseInMemoryDatabase();
-            builder.UseSqlite("Filename=submissions.db");
+            string dbconn = System.Environment.GetEnvironmentVariable("DBConnectionString");
+            //optionsBuilder.UseSqlite(dbconn); // example "filename=submissions.db"
+            optionsBuilder.UseSqlServer(dbconn);
         }
     }
 }
